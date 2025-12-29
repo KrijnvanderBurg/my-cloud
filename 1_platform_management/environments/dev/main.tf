539d037a07a021b4733d0beace615302721bc98c
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2025-12-29 17:36:43 +0000

    w

--- 1_platform_management/environments/dev/main.tf
+++ 1_platform_management/environments/dev/main.tf
@@ -91,3 +91,17 @@ data "azurerm_billing_mca_account_scope" "billing" {
   billing_profile_name = each.value.billing_profile_name
   invoice_section_name = each.value.invoice_section_name
 }
+
+# =============================================================================
+# Subscriptions
+# =============================================================================
+module "drive_subscription" {
+  source              = "../../modules/subscription"
+  name                = "pl-management-${var.environment}-na-01"
+  billing_scope_id    = data.azurerm_billing_mca_account_scope.billing["platform"].id
+  management_group_id = module.drive_management_group.id
+  tags = merge(
+    var.tags,
+    {}
+  )
+}
