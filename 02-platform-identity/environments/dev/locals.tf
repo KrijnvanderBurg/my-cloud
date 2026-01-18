201629117262c0b762bcc4f70dfdd82a29145067
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2026-01-18 15:09:16 +0000

    w

--- 02-platform-identity/environments/dev/locals.tf
+++ 02-platform-identity/environments/dev/locals.tf
@@ -4,14 +4,19 @@
 
 locals {
   # ---------------------------------------------------------------------------
-  # Remote State Outputs
+  # Remote State References
   # ---------------------------------------------------------------------------
-  tenant_root_management_group_id     = data.terraform_remote_state.management.outputs.tenant_root_management_group_id
-  tfstate_storage_account_id          = data.terraform_remote_state.management.outputs.tfstate_storage_account.id
-  alz_drive_subscription_id           = data.terraform_remote_state.management.outputs.alz_drive_subscription.id
-  pl_identity_subscription_id         = data.terraform_remote_state.management.outputs.pl_identity_subscription.subscription_id
-  pl_connectivity_subscription_id     = data.terraform_remote_state.management.outputs.pl_connectivity_subscription.id
-  pl_connectivity_management_group_id = data.terraform_remote_state.management.outputs.platform_connectivity_management_group.id
+  tenant_id                       = data.terraform_remote_state.management.outputs.tenant_id
+  tenant_root_management_group_id = data.terraform_remote_state.management.outputs.tenant_root_management_group_id
+  tfstate_storage_account_id      = data.terraform_remote_state.management.outputs.tfstate_storage_account_id
+
+  # ---------------------------------------------------------------------------
+  # Subscription IDs (hardcoded - no circular dependency)
+  # ---------------------------------------------------------------------------
+  pl_management_subscription_id   = data.terraform_remote_state.management.outputs.pl_management_subscription.subscription_id
+  pl_identity_subscription_id     = data.terraform_remote_state.management.outputs.pl_identity_subscription.subscription_id
+  pl_connectivity_subscription_id = data.terraform_remote_state.management.outputs.pl_connectivity_subscription.id
+  alz_drive_subscription_id       = data.terraform_remote_state.management.outputs.alz_drive_subscription.id
 
   # ---------------------------------------------------------------------------
   # Region Configuration
