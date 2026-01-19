c15fe08869ffad153452bfbe12bfad13907bf449
Krijn van der Burg <krijn.vander.burg@tennet.eu>
2026-01-19 08:48:44 +0000

    w

--- 03-platform-connectivity/environments/dev-glb/outputs.tf
+++ 03-platform-connectivity/environments/dev-glb/outputs.tf
@@ -6,12 +6,12 @@ output "hub_peerings" {
   description = "Hub-to-hub peering details"
   value = {
     weu_to_gwc = {
-      id            = module.hub_weu_to_hub_gwc.id
-      name          = module.hub_weu_to_hub_gwc.name
+      id   = module.hub_weu_to_hub_gwc.id
+      name = module.hub_weu_to_hub_gwc.name
     }
     gwc_to_weu = {
-      id            = module.hub_gwc_to_hub_weu.id
-      name          = module.hub_gwc_to_hub_weu.name
+      id   = module.hub_gwc_to_hub_weu.id
+      name = module.hub_gwc_to_hub_weu.name
     }
   }
 }
