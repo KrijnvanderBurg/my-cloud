c0b21238eba530222142db89002ff5d1228e4f59
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2026-01-25 15:58:03 +0000

    w

--- 05-platform-landing-zones/environments/drives-dev/main.tf
+++ 05-platform-landing-zones/environments/drives-dev/main.tf
@@ -1,6 +1,10 @@
 module "vnet-spoke" {
   source = "../../modules/01-vnet-spoke"
 
+  providers = {
+    azurerm.connectivity = azurerm.connectivity
+  }
+
   name                = "vnet-${local.landing_zone}-on-${local.environment}-${local.location_short}-01"
   resource_group_name = "rg-connectivity-${local.landing_zone}-${local.environment}-${local.location_short}-01"
   location            = local.location
@@ -8,7 +12,7 @@ module "vnet-spoke" {
 
   hub_vnet_id             = local.hub_vnet_id
   hub_vnet_name           = local.hub_vnet_name
-  use_remote_gateways     = false
+  use_remote_gateways     = false # Set to true when gateways are deployed in the hub
   hub_resource_group_name = local.hub_resource_group_name
 
   lz_managed_subnets      = local.lz_managed_subnets
