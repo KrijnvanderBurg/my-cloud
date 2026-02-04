744d579952ac46d3a5bc83f647379c2f1352a7ba
Krijn van der Burg <32356726+KrijnvanderBurg@users.noreply.github.com>
2026-02-04 06:51:06 +0000

    w

--- 02-platform-identity/modules/02b-rbac-plz/main.tf
+++ 02-platform-identity/modules/02b-rbac-plz/main.tf
@@ -2,23 +2,6 @@
 # RBAC Role Assignments - PLZ Drives SP
 # =============================================================================
 
-data "terraform_remote_state" "connectivity_weu" {
-  backend = "azurerm"
-
-  config = {
-    subscription_id      = "e388ddce-c79d-4db0-8a6f-cd69b1708954"
-    resource_group_name  = "rg-tfstate-co-dev-gwc-01"
-    storage_account_name = "sttfstatecodevgwc01"
-    container_name       = "tfstate-pl-connectivity"
-    key                  = "pl-connectivity-dev-weu.tfstate"
-    use_azuread_auth     = true
-  }
-}
-
-locals {
-  hub_vnet_id = data.terraform_remote_state.connectivity_weu.outputs.hub.id
-}
-
 # Contributor for managing PLZ drive resources
 resource "azurerm_role_assignment" "subscription_contributor" {
   scope                = var.plz_drives_subscription_scope
@@ -47,28 +30,28 @@ resource "azurerm_role_assignment" "connectivity_network_contributor" {
   principal_id         = var.principal_id
 }
 
-# Custom role for VNet peering only
-resource "azurerm_role_definition" "vnet_peering_only" {
-  name  = "VNet Peering Only - PLZ Drives"
-  scope = local.hub_vnet_id
+# # Custom role for VNet peering only
+# resource "azurerm_role_definition" "vnet_peering_only" {
+#   name  = "VNet Peering Only - PLZ Drives"
+#   scope = local.hub_vnet_id
 
-  permissions {
-    actions = [
-      "Microsoft.Network/virtualNetworks/peer/action",
-      "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/read",
-      "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/write",
-      "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/delete"
-    ]
-    not_actions = []
-  }
+#   permissions {
+#     actions = [
+#       "Microsoft.Network/virtualNetworks/peer/action",
+#       "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/read",
+#       "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/write",
+#       "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/delete"
+#     ]
+#     not_actions = []
+#   }
 
-  assignable_scopes = [
-    local.hub_vnet_id
-  ]
-}
+#   assignable_scopes = [
+#     local.hub_vnet_id
+#   ]
+# }
 
-resource "azurerm_role_assignment" "hub_vnet_peering" {
-  scope              = local.hub_vnet_id
-  role_definition_id = azurerm_role_definition.vnet_peering_only.role_definition_resource_id
-  principal_id       = var.principal_id
-}
+# resource "azurerm_role_assignment" "hub_vnet_peering" {
+#   scope              = local.hub_vnet_id
+#   role_definition_id = azurerm_role_definition.vnet_peering_only.role_definition_resource_id
+#   principal_id       = var.principal_id
+# }
