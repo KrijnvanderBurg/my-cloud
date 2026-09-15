8dd69beda8a6151656258526dcc426cb7f1c68a2
Krijn van der Burg <krijnvdburg@protonmail.com>
2026-09-15 17:07:30 +0200

    w

--- 03-ovhcloud/01-platform-management/environments/dev/locals.tf
+++ 03-ovhcloud/01-platform-management/environments/dev/locals.tf
@@ -1,4 +1,12 @@
-variable "project_id" {
-  description = "OVHcloud Public Cloud project ID for this environment."
-  type        = string
+locals {
+  project_id  = "f350cf3e972b41fca80eb0a0a1b69dbf"
+  environment = "dev"
+  common_tags = {
+    environment = local.environment
+    managed_by  = "opentofu"
+    project     = "levendaal"
+    layer       = "platform-management"
+    owner       = "kvdb"
+    cost_center = "platform"
+  }
 }
